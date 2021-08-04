ecf9e5b154bc9e96b81ac31ebb85004d99023dcc
KotaHarrison <dakota.harrison.06@gmail.com>
2021-08-03 21:04:39 -0500

    Shooting

---
+++ Elemental Rush/Assets/Scripts/Projectile.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float speed = 20f;
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.forward * speed;
+    }
+
+    private void OnTriggerEnter (Collider hitInfo)
+    {
+        Debug.Log(hitInfo.name);
+        Destroy(gameObject);
+    }
+
+}
